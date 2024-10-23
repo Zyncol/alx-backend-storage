7f97e7d38df1aac3fb322796d3acad9800e1a949
Zyncol <isaacyilipochipofya@gmail.com>
2024-10-23 18:09:14 +0200

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,12 +11,13 @@ class Cache:
     """
     Represents an object for storing data
     """
-    def __init__(self, host="localhost",port=6379, db=0):
+    def __init__(self, host="localhost", port=6379, db=0):
         """
         stores an instanc as private variable
         """
         self._redis = redis.Redis(host=host, port=port, db=db)
         self._redis.flushdb()
+
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         generates a random key
@@ -24,7 +25,8 @@ class Cache:
         keyy = str(uuid.uuid4())
         self._redis.set(keyy, data)
         return keyy
-    def get(self, key: str, 
+
+    def get(self, key: str,
             fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
         """
         takes key string arguments
@@ -42,8 +44,9 @@ class Cache:
         """
         retrieves a string value from storage
         """
-        Dvalue = self.get(key, fn=lambda x: x.decode("utf-8"))
+        Dvalue = self.get(key, fn=lambda d: d.decode("utf-8"))
         return Dvalue
+
     def get_int(self, key: str) -> int:
         """
         retrieves an integer value from storage
